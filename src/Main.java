036422b54daee99189ba97b36070906659867dc4
Pavlo <pawel.wowniuk@gmail.com>
2019-01-28 12:50:09 +0100

    Zadanie domowe 6.5(Dodaje jeszcze 1 metode)

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        EvenNumber evenNumber = new EvenNumber ();
+        int sum = evenNumber.getListOfEvenNumber();
+        System.out.println ("suma liczb parzystych = " + sum);
+    }
+}
